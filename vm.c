3f8913e3744c38bdce9277bf27f71a82576ed254
Brian Taylor <el.wubo@gmail.com>
2011-01-26 08:00:25 -0500

    removed extra switch from vm loop

--- vm.c
+++ vm.c
@@ -212,239 +212,228 @@ vm_begin:
 
   VM_DEBUG("dispatching", instr);
 
-  switch (opcode->type) {
-  case CHARACTER:
-    switch(CHAR(opcode)) {
-    case _args_: {
-      if(n_args != LONG(ARG1(instr))) {
-	VM_ASSERT(0, "wrong number of args. expected %ld, got %ld\n",
-		  LONG(ARG1(instr)), n_args);
-      }
-
-      int ii;
-      int num_args = LONG(ARG1(instr));
-
-      /* resize the top frame if we need to */
-      if(num_args > VSIZE(car(env))) {
-	set_car(env, make_vector(the_empty_list, num_args));
-      }
-
-      object *vector = car(env);
-      object **vdata = VARRAY(vector);
-      for(ii = num_args - 1; ii >= 0; --ii) {
-	VPOP(top, stack, stack_top);
-	vdata[ii] = top;
-      }
-
-      VM_DEBUG("after_args environment", env);
+  switch(CHAR(opcode)) {
+  case _args_: {
+    if(n_args != LONG(ARG1(instr))) {
+      VM_ASSERT(0, "wrong number of args. expected %ld, got %ld\n",
+		LONG(ARG1(instr)), n_args);
     }
-      break;
-    case _argsdot_: {
-      VM_ASSERT(n_args >= LONG(ARG1(instr)), "wrong number of args");
 
-      int ii;
-      long req_args = LONG(ARG1(instr));
-      long array_size = req_args + 1;
+    int ii;
+    int num_args = LONG(ARG1(instr));
 
-      /* resize the top frame if we need to */
-      if(array_size > VSIZE(car(env))) {
-	set_car(env, make_vector(the_empty_list,
-				 array_size));
-      }
-
-      object *vector = car(env);
-      object **vdata = VARRAY(vector);
-
-      /* push the excess args onto the last position, top is
-	 protected */
-      for(ii = 0; ii < n_args - req_args; ++ii) {
-	VPOP(top, stack, stack_top);
-	vdata[array_size - 1] = cons(top, vdata[array_size - 1]);
-      }
-
-      /* now pop off the required args into their positions */
-      for(ii = req_args - 1; ii >= 0; --ii) {
-	VPOP(top, stack, stack_top);
-	vdata[ii] = top;
-      }
-
-      VM_DEBUG("after_args environment", env);
+    /* resize the top frame if we need to */
+    if(num_args > VSIZE(car(env))) {
+      set_car(env, make_vector(the_empty_list, num_args));
     }
-      break;
-    case _fjump_: {
+
+    object *vector = car(env);
+    object **vdata = VARRAY(vector);
+    for(ii = num_args - 1; ii >= 0; --ii) {
       VPOP(top, stack, stack_top);
-      if(is_falselike(top)) {
-	pc = LONG(ARG1(instr));
-      }
+      vdata[ii] = top;
     }
-      break;
-    case _tjump_: {
+
+    VM_DEBUG("after_args environment", env);
+  }
+    break;
+  case _argsdot_: {
+    VM_ASSERT(n_args >= LONG(ARG1(instr)), "wrong number of args");
+
+    int ii;
+    long req_args = LONG(ARG1(instr));
+    long array_size = req_args + 1;
+
+    /* resize the top frame if we need to */
+    if(array_size > VSIZE(car(env))) {
+      set_car(env, make_vector(the_empty_list,
+			       array_size));
+    }
+
+    object *vector = car(env);
+    object **vdata = VARRAY(vector);
+
+    /* push the excess args onto the last position, top is
+       protected */
+    for(ii = 0; ii < n_args - req_args; ++ii) {
       VPOP(top, stack, stack_top);
-      if(!is_falselike(top)) {
-	pc = LONG(ARG1(instr));
-      }
+      vdata[array_size - 1] = cons(top, vdata[array_size - 1]);
     }
-      break;
-    case _jump_: {
+
+    /* now pop off the required args into their positions */
+    for(ii = req_args - 1; ii >= 0; --ii) {
+      VPOP(top, stack, stack_top);
+      vdata[ii] = top;
+    }
+
+    VM_DEBUG("after_args environment", env);
+  }
+    break;
+  case _fjump_: {
+    VPOP(top, stack, stack_top);
+    if(is_falselike(top)) {
       pc = LONG(ARG1(instr));
     }
-      break;
-    case _fn_: {
-      object *fn_arg = ARG1(instr);
-      object *new_fn = make_compiled_proc(BYTECODE(fn_arg),
-					  env);
-      push_root(&new_fn);
-      VPUSH(new_fn, stack, stack_top);
-      pop_root(&new_fn);
-    }
-      break;
-    case _fcallj_: {
-      VPOP(top, stack, stack_top);
-
-      /* unwrap meta */
-      if(is_meta(top)) {
-	top = METAPROC(top);
-      }
-
-      long args_for_call = LONG(ARG1(instr));
-
-      /* special case for apply */
-      if(args_for_call == -1) {
-	/* function is on the top of the stack (as usual) */
-	object * target_fn = top;
-	push_root(&target_fn);
-
-	/* and the args are in a list next, expand those */
-	VPOP(top, stack, stack_top);
-
-	args_for_call = 0;
-	while(!is_the_empty_list(top)) {
-	  VPUSH(car(top), stack, stack_top);
-	  top = cdr(top);
-	  ++args_for_call;
-	}
-
-	/* and put back the fn for the dispatch */
-	top = target_fn;
-	pop_root(&target_fn);
-      }
-
-      if(is_compiled_proc(top) ||
-	 is_compiled_syntax_proc(top)) {
-	fn = top;
-	env = CENV(fn);
-	pc = 0;
-	n_args = args_for_call;
-
-	/* build an environment for the receiver that's at least big
-	   enough for its args */
-	object *newframe = make_vector(the_empty_list, n_args + 1);
-	push_root(&newframe);
-	env = cons(newframe, env);
-	pop_root(&newframe);
-
-	goto vm_fn_begin;
-      } else if(is_primitive_proc(top)) {
-	/* build the list the target expects for the call */
-	long ii;
-	object *pfn = top;
-	push_root(&pfn);
-
-	top = pfn->data.primitive_proc.fn(stack, args_for_call, stack_top);
-	/* unwind the stack since primitives don't clean up after
-	   themselves */
-	object *temp;
-	for(ii = 0; ii < args_for_call; ++ii) {
-	  VPOP(temp, stack, stack_top);
-	}
-
-	VPUSH(top, stack, stack_top);
-	pop_root(&pfn);
-
-	RETURN_OPCODE_INSTRUCTIONS;
-      } else {
-	owrite(stderr, top);
-	fprintf(stderr, "\n");
-	VM_ASSERT(0, "don't know how to invoke");
-      }
-    }
-      break;
-    case _lvar_: {
-      int env_num = LONG(ARG1(instr));
-      int idx = LONG(ARG2(instr));
-
-      object *next = env;
-      while(env_num-- > 0) {
-	next = cdr(next);
-      }
-
-      object *data = VARRAY(car(next))[idx];
-      VPUSH(data, stack, stack_top);
-    }
-      break;
-    case _lset_: {
-      int env_num = LONG(ARG1(instr));
-      int idx = LONG(ARG2(instr));
-
-      object *next = env;
-      while(env_num-- > 0) {
-	next = cdr(next);
-      }
-
-      VARRAY(car(next))[idx] = VARRAY(stack)[stack_top - 1];
-    }
-      break;
-    case _gvar_: {
-      object *var = lookup_global_value(ARG1(instr), vm_global_environment);
-      push_root(&var);
-      VPUSH(var, stack, stack_top);
-      pop_root(&var);
-    }
-      break;
-    case _gset_: {
-      object *var = ARG1(instr);
-      object *val = VARRAY(stack)[stack_top - 1];
-      define_global_variable(var, val, vm_global_environment);
-    }
-      break;
-    case _pop_: {
-      VPOP(top, stack, stack_top);
-    }
-      break;
-    case _save_: {
-      object *ret_addr = cons(fn, env);
-      push_root(&ret_addr);
-      ret_addr = cons(ARG1(instr), ret_addr);
-      VPUSH(ret_addr, stack, stack_top);
-      pop_root(&ret_addr);
-    }
-      break;
-    case _return_: {
-      RETURN_OPCODE_INSTRUCTIONS;
-    }
-      break;
-    case _const_: {
-      VPUSH(ARG1(instr), stack, stack_top);
-    }
-      break;
-    default: {
-      fprintf(stderr, "don't know how to process ");
-      owrite(stderr, opcode);
-      fprintf(stderr, "\n");
-      VM_ASSERT(0, "strange opcode");
-    }
-    }
+  }
     break;
+  case _tjump_: {
+    VPOP(top, stack, stack_top);
+    if(!is_falselike(top)) {
+      pc = LONG(ARG1(instr));
+    }
+  }
+    break;
+  case _jump_: {
+    pc = LONG(ARG1(instr));
+  }
+    break;
+  case _fn_: {
+    object *fn_arg = ARG1(instr);
+    object *new_fn = make_compiled_proc(BYTECODE(fn_arg),
+					env);
+    push_root(&new_fn);
+    VPUSH(new_fn, stack, stack_top);
+    pop_root(&new_fn);
+  }
+    break;
+  case _fcallj_: {
+    VPOP(top, stack, stack_top);
 
-  default:
+    /* unwrap meta */
+    if(is_meta(top)) {
+      top = METAPROC(top);
+    }
+
+    long args_for_call = LONG(ARG1(instr));
+
+    /* special case for apply */
+    if(args_for_call == -1) {
+      /* function is on the top of the stack (as usual) */
+      object * target_fn = top;
+      push_root(&target_fn);
+
+      /* and the args are in a list next, expand those */
+      VPOP(top, stack, stack_top);
+
+      args_for_call = 0;
+      while(!is_the_empty_list(top)) {
+	VPUSH(car(top), stack, stack_top);
+	top = cdr(top);
+	++args_for_call;
+      }
+
+      /* and put back the fn for the dispatch */
+      top = target_fn;
+      pop_root(&target_fn);
+    }
+
+    if(is_compiled_proc(top) ||
+       is_compiled_syntax_proc(top)) {
+      fn = top;
+      env = CENV(fn);
+      pc = 0;
+      n_args = args_for_call;
+
+      /* build an environment for the receiver that's at least big
+	 enough for its args */
+      object *newframe = make_vector(the_empty_list, n_args + 1);
+      push_root(&newframe);
+      env = cons(newframe, env);
+      pop_root(&newframe);
+
+      goto vm_fn_begin;
+    } else if(is_primitive_proc(top)) {
+      /* build the list the target expects for the call */
+      long ii;
+      object *pfn = top;
+      push_root(&pfn);
+
+      top = pfn->data.primitive_proc.fn(stack, args_for_call, stack_top);
+      /* unwind the stack since primitives don't clean up after
+	 themselves */
+      object *temp;
+      for(ii = 0; ii < args_for_call; ++ii) {
+	VPOP(temp, stack, stack_top);
+      }
+
+      VPUSH(top, stack, stack_top);
+      pop_root(&pfn);
+
+      RETURN_OPCODE_INSTRUCTIONS;
+    } else {
+      owrite(stderr, top);
+      fprintf(stderr, "\n");
+      VM_ASSERT(0, "don't know how to invoke");
+    }
+  }
+    break;
+  case _lvar_: {
+    int env_num = LONG(ARG1(instr));
+    int idx = LONG(ARG2(instr));
+
+    object *next = env;
+    while(env_num-- > 0) {
+      next = cdr(next);
+    }
+
+    object *data = VARRAY(car(next))[idx];
+    VPUSH(data, stack, stack_top);
+  }
+    break;
+  case _lset_: {
+    int env_num = LONG(ARG1(instr));
+    int idx = LONG(ARG2(instr));
+
+    object *next = env;
+    while(env_num-- > 0) {
+      next = cdr(next);
+    }
+
+    VARRAY(car(next))[idx] = VARRAY(stack)[stack_top - 1];
+  }
+    break;
+  case _gvar_: {
+    object *var = lookup_global_value(ARG1(instr), vm_global_environment);
+    push_root(&var);
+    VPUSH(var, stack, stack_top);
+    pop_root(&var);
+  }
+    break;
+  case _gset_: {
+    object *var = ARG1(instr);
+    object *val = VARRAY(stack)[stack_top - 1];
+    define_global_variable(var, val, vm_global_environment);
+  }
+    break;
+  case _pop_: {
+    VPOP(top, stack, stack_top);
+  }
+    break;
+  case _save_: {
+    object *ret_addr = cons(fn, env);
+    push_root(&ret_addr);
+    ret_addr = cons(ARG1(instr), ret_addr);
+    VPUSH(ret_addr, stack, stack_top);
+    pop_root(&ret_addr);
+  }
+    break;
+  case _return_: {
+    RETURN_OPCODE_INSTRUCTIONS;
+  }
+    break;
+  case _const_: {
+    VPUSH(ARG1(instr), stack, stack_top);
+  }
+    break;
+  default: {
     fprintf(stderr, "don't know how to process ");
     owrite(stderr, opcode);
     fprintf(stderr, "\n");
     VM_ASSERT(0, "strange opcode");
   }
+  }
 
   goto vm_begin;
-
   VM_RETURN(error_sym);
 }
 
