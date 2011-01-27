b9fd1767321751febe0cfdceb0b2474dc79f60ee
Christopher Wellons <mosquitopsu@gmail.com>
2011-01-26 19:27:00 -0500

    Begun adding BS_PATH support.

--- bsch.c
+++ bsch.c
@@ -16,17 +16,74 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "interp.h"
 #include "read.h"
 #include "gc.h"
 #include "ffi.h"
 
+char **bs_paths;
+
+char **split_path(char *path) {
+  /* Count delimiters while making a copy. */
+  char del = ':';
+  int count = 1;
+  char *copy = MALLOC(strlen(path) + 1), *cp = copy, *p = path;
+  while (*p != '\0') {
+    *cp = *p;
+    if (*p == del)
+      count++;
+    cp++;
+    p++;
+  }
+  *cp = '\0';
+
+  cp = copy;
+  char **r = MALLOC((count + 1) * sizeof(char**));
+  int i;
+  for (i = 0; i < count; i++) {
+    r[i] = cp;
+    cp = strchr(cp, del);
+    if (cp != NULL)
+      *cp = '\0';
+    cp++;
+  }
+  r[count] = NULL;
+  return r;
+}
+
+/* Allocate new string and concat strings in it separated by a /. */
+char *pathcat(char *a, char *b) {
+  size_t alen = strlen(a);
+  size_t blen = strlen(b);
+  char *out = MALLOC(alen + blen + 2);
+  strcpy(out, a);
+  *(out + alen) = '/';
+  strcpy(out + alen + 1, b);
+  *(out + alen + blen + 1) = '\0';
+  return out;
+}
+
 void load_library(char *libname) {
-  FILE * stdlib = fopen(libname, "r");
+  char *filename;
+  char **paths = bs_paths;
+  while (*paths != NULL) {
+    filename = pathcat(*paths, libname);
+    printf("checking %s\n", filename);
+    if (access(filename, R_OK) == 0)
+      break;
+    free(filename);
+    filename = NULL;
+    paths++;
+  }
+  if(filename == NULL) {
+    fprintf(stderr, "Failed to load %s. Is BS_PATH right?\n", libname);
+    exit(3);
+  }
+  FILE * stdlib = fopen(filename, "r");
   if(stdlib == NULL) {
-    fprintf(stderr, "failed to load %s. Is CWD right?\n",
-	    libname);
+    fprintf(stderr, "Somehow failed to load %s after it existed.\n", libname);
     exit(3);
   }
 
@@ -45,6 +102,11 @@ int main(int argc, char ** argv) {
   char *libname = "stdlib.sch";
 
   init();
+
+  char *path = getenv("BS_PATH");
+  if (path == NULL)
+    path = ".";
+  bs_paths = split_path(path);
 
   if(argc > 1 && strcmp(argv[1], "-b") == 0) {
     /* don't load the standard lib */
